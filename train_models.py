4a03bcaae7f56341db8d18250938277689dee9cd
jeremy <jbarnesspain@gmail.com>
2021-03-15 09:13:33 +0100

    parameterized

--- train_models.py
+++ train_models.py
@@ -7,20 +7,28 @@ from sklearn.metrics import f1_score
 
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.svm import LinearSVC
+from time import time
+
+import argparse
 
 def svm_predict_text(svm_model, vectorizer, text):
     return svm_model.predict(vectorizer.transform([text]))[0]
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--training_epochs", type=int, default=10)
+    parser.add_argument("--batch_size", type=int, default=32)
+    parser.add_argument("--embedding_dim", type=int, default=50)
+    parser.add_argument("--hidden_dim", type=int, default=50)
+    parser.add_argument("--training_data", default="data/train_small.tsv")
     # setup hyperparameters
-    training_epochs = 10
-    training_data = "data/train_medium.tsv"
+    args = parser.parse_args()
 
     # Load the data and create the vocabulary at the same time
-    # The Vocab object creates a new entry for each new word
-    # that it sees while loading the datasets
+    # The Vocab object is a default dictionary that creates a new entry
+    # for each new word that it sees while loading the datasets
     vocab = Vocab()
-    train_data = SentimentDataset(training_data, vocab).get_split()
+    train_data = SentimentDataset(args.training_data, vocab).get_split()
     dev_data = SentimentDataset("data/dev.tsv", vocab).get_split()
     test_data = [l.strip().split("\t") for l in open("data/test.tsv")]
     vocab.eval()
@@ -28,8 +36,11 @@ if __name__ == "__main__":
     ###################################################################
     # BOW SETUP
     ###################################################################
+
+    # we create a sklearn vectorizer which is initialized with the same
+    # vocabulary as we will use for the LSTM, making the comparison more fair
     vectorizer = CountVectorizer(vocabulary=vocab)
-    train_y, train_text = zip(*[l.strip().split("\t") for l in open(training_data)])
+    train_y, train_text = zip(*[l.strip().split("\t") for l in open(args.training_data)])
     bow_train = vectorizer.transform(train_text)
     dev_y, dev_text = zip(*[l.strip().split("\t") for l in open("data/dev.tsv")])
     bow_dev = vectorizer.transform(dev_text)
@@ -38,7 +49,13 @@ if __name__ == "__main__":
 
     print("Training Linear SVM on Bag of Words representations...")
     svm_model = LinearSVC()
+    start_time = time()
     svm_model.fit(bow_train, train_y)
+    end_time = time()
+
+    print("{0:.1f} seconds to train".format(end_time - start_time))
+    print()
+
     pred = svm_model.predict(bow_train)
     score = f1_score(train_y, pred, average="macro")
     print("Train F1: {0:.3f}".format(score), end="\t")
@@ -50,9 +67,9 @@ if __name__ == "__main__":
     ###################################################################
     # LSTM SETUP
     ###################################################################
-    print("Training LSTM...")
+    print("Training LSTM on {} examples...".format(len(train_data)))
     train_loader = DataLoader(train_data,
-                              batch_size=32,
+                              batch_size=args.batch_size,
                               collate_fn=train_data.collate_fn,
                               shuffle=True)
 
@@ -62,16 +79,17 @@ if __name__ == "__main__":
                             shuffle=False)
 
     model = LSTM_Model(word2idx=vocab,
-                       embedding_dim=50,
-                       hidden_dim=50,
+                       embedding_dim=args.embedding_dim,
+                       hidden_dim=args.hidden_dim,
                        num_labels=2)
 
-    # use binary cross entropy as the loss function
+    # use cross entropy as the loss function
     loss_function = nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters())
 
     # For each epoch in training epochs
-    for i, epoch in enumerate(range(training_epochs)):
+    start_time = time()
+    for i, epoch in enumerate(range(args.training_epochs)):
         model.train()
         print("Epoch {0}    ".format(i), end="\t")
         epoch_loss = 0
@@ -85,7 +103,7 @@ if __name__ == "__main__":
             loss.backward()
             optimizer.step()
             model.zero_grad()
-        print("Loss: {0:.3f}".format(loss / training_epochs), end="\t")
+        print("Loss: {0:.3f}".format(loss / args.training_epochs), end="\t")
 
         # check results on train
         train_preds, train_gold = [], []
@@ -110,9 +128,15 @@ if __name__ == "__main__":
             dev_gold.extend(labels.flatten().numpy())
         score = f1_score(dev_gold, dev_preds, average="macro")
         print("Dev F1: {0:.3f}".format(score))
+    end_time = time()
+
+    print("{0:.1f} seconds to train".format(end_time - start_time))
+    print()
 
     model.eval()
 
+
+    # Now we can evaluate both models on the test data
     print()
     print("Evaluating on the challenge data...")
     print()
